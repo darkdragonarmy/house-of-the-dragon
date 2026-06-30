241540c6450d6f81d81774d614a1f98ff79536a0
Michelle Leblanc <HOTD2026@hotmail.com>
2026-06-30 18:48:23 -0400

    Fix empty royal court page

---
+++ app/royal-court/page.tsx
@@ -0,0 +1,8 @@
+export default function RoyalCourtPage() {
+  return (
+    <main style={{ padding: "4rem", color: "white" }}>
+      <h1>Royal Court</h1>
+      <p>This section is currently under construction.</p>
+    </main>
+  );
+}
